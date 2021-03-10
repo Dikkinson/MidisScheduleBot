0b31ab1ac1410c5ec1cc8a9754533a6c4084f830
Dikkinson <Dikkinson@yandex.ru>
2021-03-10 20:48:32 +0500

    Обновление ip

--- loader.py
+++ loader.py
@@ -4,7 +4,7 @@ from aiogram.contrib.fsm_storage.redis import RedisStorage
 from data import config
 from utils.misc.parser import Parser
 
-parser = Parser('Расписание на 05.03.21.xlsx')
+parser = Parser('Расписание на 11.03.21.xlsx')
 rasp = parser.get_rasp()
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = RedisStorage()
