97347ccca390f995d2ed319aea7b7cad47d6b10d
wifelly <supermegawifi@gmail.com>
2021-03-13 15:03:49 +0500

    конфиг

--- data/config.py
+++ data/config.py
@@ -7,3 +7,12 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
+
+mysql_info = {
+    'host':     'localhost',
+    'user':     env.str("db_user"),
+    'password': env.str("db_pass"),
+    'db':       env.str("db"),
+    'maxsize':  5,
+    'port':     3306
+}
